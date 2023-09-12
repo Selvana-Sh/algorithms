04cba72821b9656c070b4166d572ac2baa425262
Selvana-Sh <selvanamoinshaghaghi@gmail.com>
2023-09-12 22:15:48 +0400

    update quick sort

---
+++ sort/quick_sort.py
@@ -0,0 +1,25 @@
+# takes an unsorted sequence and sorts it out in ascending or descending order
+# pivot : just a number we base our comparisons off of
+
+# complexity : average-case O(n log n), worst-case O(n^2)
+
+def quick_sort(seq):
+    length = len(seq)
+    if length <= 1:
+        return seq
+    else:
+        pivot = seq[length//2]
+
+    lower = []
+    higher = []
+    the_pivot = []
+
+    for item in seq:
+        if item < pivot:
+           lower.append(item)
+        elif item > pivot:
+           higher.append(item)
+        else:
+           the_pivot.append(item)
+
+    return quick_sort(lower) + the_pivot + quick_sort(higher)
