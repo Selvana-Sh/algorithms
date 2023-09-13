8ae5379fda03c44dfe60c1fa1ec0dd964a6b32ef
Selvana <90338618+Selvana-Sh@users.noreply.github.com>
2023-09-13 19:00:20 +0400

    update bubble_sort.py

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -2,15 +2,15 @@
 # sort in ascending order 
 # complexity: average and worst-case: O(n^2)
 
-def bubble(seq):
-    n = len(seq)-1 # to not swap the last element
+def bubble(arr):
+    n = len(arr)-1 # to not swap the last element
     swapped = True  # enter the loop
     
     while swapped:
         swapped = False  # reset for every iteration
         
         for i in range(1, n):
-            if seq[i] > seq[i+1]:
-                seq[i], seq[i+1] = seq[i+1], seq[i]
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
                 swapped = True # continue the loop 
-    return seq
+    return arr
